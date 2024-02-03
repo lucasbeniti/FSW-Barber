75819dcc24ba7037c810ebbb35b1c752e260ec92
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2024-02-02 22:51:25 -0300

    feat: add search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
-import Header from "./_components/header";
+import Header from "../components/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
@@ -14,6 +15,10 @@ export default function Home() {
             locale: ptBR,
           })}
         </p>
+      </div>
+
+      <div className="px-5">
+        <Search />
       </div>
     </div>
   );
