82ff1914d68525f2c7154cea07387856cf87bd72
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2024-02-04 12:33:13 -0300

    feat: add initial service booking (sheet calendar)

---
+++ app/barbershops/[id]/_helpers/hours.ts
@@ -0,0 +1,16 @@
+import { addMinutes, setHours, setMinutes, format } from "date-fns";
+
+export function generateDayTimeList(date: Date): string[] {
+  const startTime = setMinutes(setHours(date, 9), 0); // Seta a hr do início para 09:00;
+  const endTime = setMinutes(setHours(date, 21), 0); // Seta a hr do fim para 21:00;
+  const interval = 45;
+  const timeList: string[] = [];
+
+  let currentTime = startTime;
+  while (currentTime <= endTime) {
+    timeList.push(format(currentTime, "HH:mm"));
+    currentTime = addMinutes(currentTime, interval);
+  }
+
+  return timeList;
+}
