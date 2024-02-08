d942fd6e6d04734c3fd60e23d1aada3dd957350c
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2024-02-08 18:40:31 -0300

    feat: preparing for deploy

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -3,7 +3,7 @@ import BarbershopInfo from "./_components/barbershop-info";
 import { Button } from "@/app/components/ui/button";
 import ServiceItem from "./_components/service-item";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/lib/auth";
 
 interface BarbershopDetailsPageProps {
   params: { id?: string };
--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import Header from "../components/header";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/lib/auth";
 import { db } from "../lib/prisma";
 import BookingItem from "../components/booking-item";
 
