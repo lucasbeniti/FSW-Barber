6cfd8fa746afd96a73c9b0d0e158ba52039d809c
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2024-02-08 19:16:25 -0300

    fix: ensure user cant book a service in a day that is already over that hour

--- app/barbershops/[id]/_helpers/hours.ts
+++ app/barbershops/[id]/_helpers/hours.ts
@@ -1,7 +1,11 @@
 import { addMinutes, setHours, setMinutes, format } from "date-fns";
 
 export function generateDayTimeList(date: Date): string[] {
-  const startTime = setMinutes(setHours(date, 9), 0); // Seta a hr do início para 09:00;
+  const startTime =
+    new Date().getHours() >= 9
+      ? setMinutes(setHours(date, new Date().getHours() + 1), 0)
+      : setMinutes(setHours(date, 9), 0);
+
   const endTime = setMinutes(setHours(date, 21), 0); // Seta a hr do fim para 21:00;
   const interval = 45;
   const timeList: string[] = [];
