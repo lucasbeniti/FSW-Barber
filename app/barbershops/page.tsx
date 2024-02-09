44b7f4e2d3985ce5430be0759dee710663020dbc
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2024-02-08 22:07:58 -0300

    fix: add default values on input

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -1,6 +1,8 @@
+import { redirect } from "next/navigation";
 import BarbershopItem from "../(home)/_components/barbershop-item";
 import Header from "../components/header";
 import { db } from "../lib/prisma";
+import Search from "../(home)/_components/search";
 
 interface BarbershopPagesProps {
   searchParams: {
@@ -9,6 +11,9 @@ interface BarbershopPagesProps {
 }
 
 const BarbershopPages = async ({ searchParams }: BarbershopPagesProps) => {
+  if (!searchParams.search) {
+    redirect("/");
+  }
   const barbershops = await db.barbershop.findMany({
     where: {
       name: {
@@ -20,10 +25,12 @@ const BarbershopPages = async ({ searchParams }: BarbershopPagesProps) => {
   return (
     <>
       <Header />
-      <div className="px-5 py-6">
+
+      <div className="px-5 py-6 flex flex-col gap-6">
+        <Search defaultValues={{ search: searchParams.search }} />
         <h1 className="text-gray-400 font-bold text-xs">
           Resultados para &quot;{searchParams.search}&quot;
-          <div className="grid grid-cols-2 mt-3 gap-4">
+          <div className="grid grid-cols-2 mt-2 gap-4">
             {barbershops.map((barbershop) => (
               <div key={barbershop.id} className="w-full">
                 <BarbershopItem barbershop={barbershop} />
